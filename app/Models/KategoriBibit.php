23f85e6710041db8bef198684800b508a8e7babb
fathan <fathan.x.a.m@gmail.com>
2025-05-08 20:09:59 +0700

    membuat model kategori bibit

---
+++ app/Models/KategoriBibit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriBibit extends Model
+{
+    protected $table = "kategori_bibit";
+    protected $fillable = ["nama_kategori"];
+}
