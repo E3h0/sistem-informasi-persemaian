28c4ab1e90a09d7d76aa62fdbcbf2f7ae6062efc
fathan <fathan.x.a.m@gmail.com>
2025-05-09 17:21:52 +0700

    membuat model blok penanaman

---
+++ app/Models/BlokPenanaman.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlokPenanaman extends Model
+{
+    protected $table = "blok_penanaman";
+    protected $fillable = ["nama_blok"];
+}
