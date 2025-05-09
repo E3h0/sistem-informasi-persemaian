7eb940e22e498fad2d2796378512d0b49fdb2f3d
fathan <fathan.x.a.m@gmail.com>
2025-05-09 17:15:21 +0700

    membuat model Area Penanaman

---
+++ app/Models/AreaPenanaman.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AreaPenanaman extends Model
+{
+    protected $table = "area_penanaman";
+    protected $fillable = ["nama_area"];
+}
