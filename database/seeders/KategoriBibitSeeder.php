c9a37efa3a013317cbbb3c7ee34d7d0128cc93d3
fathan <fathan.x.a.m@gmail.com>
2025-05-09 19:22:07 +0700

    membuat Kategori Bibit seeder

---
+++ database/seeders/KategoriBibitSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\KategoriBibit;
+
+class KategoriBibitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                "nama_kategori" => "Kayu-kayuan"
+            ],
+            [
+                "nama_kategori" => "Estetika"
+            ],
+            [
+                "nama_kategori" => "Hasil Hutan Non Kayu"
+            ],
+        ];
+
+        KategoriBibit::insert($data);
+    }
+}
