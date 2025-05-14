09dbaedbc154ecef523bb0718934716a630afa4b
fathan <fathan.x.a.m@gmail.com>
2025-05-14 07:17:42 +0700

    membuat DatabaseSeeder dan Test case

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            KategoriBibitSeeder::class,
+            PersediaanBibitSeeder::class,
+            TargetProduksiSeeder::class,
+            AreaPenanamanSeeder::class,
+            BlokPenanamanSeeder::class,
+            MutasiBibitSeeder::class,
         ]);
     }
 }
--- tests/Feature/SeederTest.php
+++ tests/Feature/SeederTest.php
@@ -10,6 +10,7 @@ use App\Models\PersediaanBibit;
 use App\Models\TargetProduksi;
 use Database\Seeders\AreaPenanamanSeeder;
 use Database\Seeders\BlokPenanamanSeeder;
+use Database\Seeders\DatabaseSeeder;
 use Database\Seeders\MutasiBibitSeeder;
 use Database\Seeders\TargetProduksiSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -78,5 +79,22 @@ test("Memastikan MutasiBibitSeeder dapat berjalan dengan baik", function () {
 
     $this->seed(MutasiBibitSeeder::class);
 
+    expect(MutasiBibit::count())->toBeGreaterThan(0);
+});
+
+test("Memastikan DatabaseSeeder dapat berjalan dengan baik", function() {
+
+    DB::table("kategori_bibit")->truncate();
+    DB::table("persediaan_bibit")->truncate();
+    DB::table("area_penanaman")->truncate();
+    DB::table("blok_penanaman")->truncate();
+
+    $this->seed(DatabaseSeeder::class);
+    
+    expect(KategoriBibit::count())->toBeGreaterThan(0);
+    expect(PersediaanBibit::count())->toBeGreaterThan(0);
+    expect(TargetProduksi::count())->toBeGreaterThan(0);
+    expect(AreaPenanaman::count())->toBeGreaterThan(0);
+    expect(BlokPenanaman::count())->toBeGreaterThan(0);
     expect(MutasiBibit::count())->toBeGreaterThan(0);
 });
