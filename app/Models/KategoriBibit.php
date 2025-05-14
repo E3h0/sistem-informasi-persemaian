26637c61635da1bd8f141341e624b96322fcd680
fathan <fathan.x.a.m@gmail.com>
2025-05-14 10:08:14 +0700

    mendefinisikan relasi antara KategoriBibit dengan PersediaanBibit

--- app/Models/KategoriBibit.php
+++ app/Models/KategoriBibit.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KategoriBibit extends Model
 {
     protected $table = "kategori_bibit";
     protected $fillable = ["nama_kategori"];
+
+    public function persediaanBibit(): HasMany {
+        return $this->hasMany(PersediaanBibit::class, "kategori_bibit_id", "id");
+    }
 }
--- app/Models/PersediaanBibit.php
+++ app/Models/PersediaanBibit.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PersediaanBibit extends Model
 {
     protected $table = "persediaan_bibit";
     protected $fillable = ["jenis_bibit", "kategori_bibit_id", "jumlah_persediaan", "keterangan"];
+
+    public function kategori(): BelongsTo {
+        return $this->belongsTo(KategoriBibit::class, "kategori_bibit_id");
+    }
 }
