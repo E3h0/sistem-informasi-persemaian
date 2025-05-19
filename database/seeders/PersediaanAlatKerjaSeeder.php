2d201192928966303e61634bf2116c8b4ebde35c
fathan <fathan.x.a.m@gmail.com>
2025-05-19 10:21:51 +0700

    membuat PersediaanAlatKerjaSeeder

---
+++ database/seeders/PersediaanAlatKerjaSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PersediaanAlatKerja;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PersediaanAlatKerjaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        PersediaanAlatKerja::create([
+            'nama_barang' => 'Cangkul',
+            'kategori_id' => 1,
+            'jumlah_persediaan' => 10,
+            'jumlah_dipakai' => 8,
+            'keterangan' => 'rusak 2 buah'
+        ]);
+    }
+}
