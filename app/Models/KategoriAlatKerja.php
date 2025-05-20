4165de889e1cb1df3b8a3fd28f4f969e850f4653
fathan <fathan.x.a.m@gmail.com>
2025-05-20 14:52:18 +0700

    Mendefinisikan relasi model PersediaanAlatKerja

--- app/Models/KategoriAlatKerja.php
+++ app/Models/KategoriAlatKerja.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KategoriAlatKerja extends Model
 {
     protected $table = 'kategori_alat_kerja';
     protected $fillable = ['nama_kategori'];
+
+    public function persediaan(): HasMany {
+        return $this->hasMany(PersediaanAlatKerja::class, 'kategori_id', 'id');
+    }
 }
--- app/Models/PersediaanAlatKerja.php
+++ app/Models/PersediaanAlatKerja.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PersediaanAlatKerja extends Model
 {
     protected $table = 'persediaan_alat_kerja';
     protected $guarded = [];
+
+    public function kategori(): BelongsTo {
+        return $this->belongsTo(KategoriAlatKerja::class, 'kategori_id');
+    }
 }
