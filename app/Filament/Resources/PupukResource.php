9668b3aec274fff502c54e89800acfa69bb02107
fathan <fathan.x.a.m@gmail.com>
2025-05-25 18:42:57 +0700

    Memperbaiki PupukResource

--- app/Filament/Resources/PupukResource.php
+++ app/Filament/Resources/PupukResource.php
@@ -12,6 +12,7 @@ use App\Models\SatuanPupuk;
 use App\Models\KategoriPupuk;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
@@ -44,18 +45,88 @@ class PupukResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('nama_pupuk')->label('Nama Pupuk')->placeholder('Masukkan nama pupuk')->required(),
+                TextInput::make('nama_pupuk')
+                    ->label('Nama Pupuk')->placeholder('Masukkan nama pupuk')
+                    ->rules(['required'])->validationMessages([
+                        'required' => 'Tolong isi bagian ini.',
+                    ])->markAsRequired(),
 
-                Select::make('satuan_pupuk_id')->label('Satuan')->placeholder('Pilih satuan pupuk')
-                ->options(SatuanPupuk::all()->pluck('nama_satuan', 'id'))->required(),
+                Select::make('satuan_pupuk_id')->label('Satuan')
+                    ->placeholder('Pilih satuan pupuk')
+                    ->relationship('satuan', 'nama_satuan')
+                    ->createOptionForm([
+                    TextInput::make('nama_satuan')
+                        ->required()
+                        ->label('Nama Satuan')->placeholder('Masukkan Nama Satuan')
+                        ->rules(['min:3'])->validationMessages([
+                            'min' => 'Minimal harus 3 karakter'
+                        ])
+                ])->createOptionModalHeading('Tambah Satuan')
+                ->createOptionUsing(function (array $data) {
+                    SatuanPupuk::create($data);
+                    Notification::make()
+                    ->title('Sukses')
+                    ->body('Satuan baru berhasil ditambahkan.')
+                    ->success()
+                    ->seconds(3)
+                    ->send();
+                })->rules(['required'])->validationMessages([
+                        'required' => 'Tolong isi bagian ini.',
+                    ])->markAsRequired(),
 
-                Select::make('bentuk_pupuk_id')->label('Bentuk')->placeholder('Pilih bentuk pupuk')
-                ->options(BentukPupuk::all()->pluck('nama_bentuk', 'id'))->required(),
+                Select::make('bentuk_pupuk_id')
+                    ->label('Bentuk')->placeholder('Pilih bentuk pupuk')
+                    ->relationship('bentuk', 'nama_bentuk')
+                    ->createOptionForm([
+                    TextInput::make('nama_bentuk')
+                        ->required()
+                        ->label('Nama Bentuk')->placeholder('Masukkan Nama Bentuk')
+                        ->rules(['min:3'])->validationMessages([
+                            'min' => 'Minimal harus 3 karakter'
+                        ])
+                ])->createOptionModalHeading('Tambah Bentuk')
+                ->createOptionUsing(function (array $data) {
+                    BentukPupuk::create($data);
+                    Notification::make()
+                    ->title('Sukses')
+                    ->body('Bentuk baru berhasil ditambahkan.')
+                    ->success()
+                    ->seconds(3)
+                    ->send();
+                })->rules(['required'])->validationMessages([
+                        'required' => 'Tolong isi bagian ini.',
+                    ])->markAsRequired(),
 
-                Select::make('kategori_pupuk_id')->label('Kategori')->placeholder('Pilih kategori pupuk')
-                ->options(KategoriPupuk::all()->pluck('nama_kategori', 'id'))->required(),
+                Select::make('kategori_pupuk_id')
+                    ->label('Kategori')->placeholder('Pilih kategori pupuk')
+                    ->relationship('kategori', 'nama_kategori')
+                    ->createOptionForm([
+                    TextInput::make('nama_kategori')
+                        ->required()
+                        ->label('Nama Kategori')->placeholder('Masukkan Nama Kategori')
+                        ->rules(['min:3'])->validationMessages([
+                            'min' => 'Minimal harus 3 karakter'
+                        ])
+                ])->createOptionModalHeading('Tambah Kategori')
+                ->createOptionUsing(function (array $data) {
+                    KategoriPupuk::create($data);
+                    Notification::make()
+                    ->title('Sukses')
+                    ->body('Kategori baru berhasil ditambahkan.')
+                    ->success()
+                    ->seconds(3)
+                    ->send();
+                })->rules(['required'])->validationMessages([
+                        'required' => 'Tolong isi bagian ini.',
+                    ])->markAsRequired(),
 
-                TextInput::make('jumlah_persediaan')->numeric()->label('Jumlah Persediaan')->placeholder('Masukkan jumlah persediaan')->required(),
+                TextInput::make('jumlah_persediaan')
+                    ->numeric()->label('Jumlah Persediaan')->placeholder('Masukkan jumlah persediaan')
+                    ->rules(['required'])->validationMessages([
+                            'required' => 'Tolong isi bagian ini.',
+                        ])->markAsRequired(),
+
+                 Textarea::make("keterangan")->placeholder("Tambahkan Keterangan")
             ]);
     }
 
@@ -63,12 +134,34 @@ class PupukResource extends Resource
     {
         return $table
             ->emptyStateHeading('Belum ada data')->emptyStateDescription('Silahkan tambahkan data terlebih dahulu.')->emptyStateIcon('heroicon-o-exclamation-circle')
+            ->recordUrl(false)
             ->columns([
-                TextColumn::make('nama_pupuk')->label('Nama Pupuk')->alignCenter(),
-                TextColumn::make('bentuk.nama_bentuk')->label('Bentuk')->alignCenter(),
-                TextColumn::make('kategori.nama_kategori')->label('Kategori')->alignCenter(),
-                TextColumn::make('jumlah_persediaan')->label('Jumlah Persediaan')->alignCenter(),
-                TextColumn::make('satuan.nama_satuan')->label('Satuan')->alignCenter(),
+                TextColumn::make('nama_pupuk')
+                    ->label('Nama Pupuk')
+                    ->searchable()->sortable(),
+
+                TextColumn::make('bentuk.nama_bentuk')
+                    ->label('Bentuk')
+                    ->searchable()->sortable(),
+
+                TextColumn::make('kategori.nama_kategori')
+                    ->label('Kategori')
+                    ->searchable()->sortable(),
+
+                TextColumn::make('jumlah_persediaan')
+                    ->label('Jumlah Persediaan')
+                    ->searchable()->sortable(),
+
+                TextColumn::make('satuan.nama_satuan')
+                    ->label('Satuan'),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat Pada')->dateTime('l, j M Y')
+                    ->sortable(),
+
+                TextColumn::make('keterangan')->label('Keterangan')
+                    ->placeholder('Tidak ada keterangan yang ditambahkan.')
+                    ->toggleable(isToggledHiddenByDefault:true)
             ])
             ->filters([
                 //
