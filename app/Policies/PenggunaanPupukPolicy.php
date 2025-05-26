0ee14c18923bfb2e9f70b801d4e26fc0dfd7b5c1
fathan <fathan.x.a.m@gmail.com>
2025-05-26 08:19:32 +0700

    Memperbaiki PenggunaanPupukPolicy

---
+++ app/Policies/PenggunaanPupukPolicy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\PenggunaanPupuk;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PenggunaanPupukPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->isAdmin() || $user->isEditor() || $user->isViewer();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, PenggunaanPupuk $penggunaanPupuk): bool
+    {
+        return $user->isAdmin() || $user->isEditor() || $user->isViewer();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, PenggunaanPupuk $penggunaanPupuk): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, PenggunaanPupuk $penggunaanPupuk): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, PenggunaanPupuk $penggunaanPupuk): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, PenggunaanPupuk $penggunaanPupuk): bool
+    {
+        return $user->isAdmin();
+    }
+
+     public function deleteAny(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+}
