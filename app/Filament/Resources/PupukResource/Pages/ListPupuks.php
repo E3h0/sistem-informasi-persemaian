e0820a115ba247be71b40baaa8b937754762a89b
fathan <fathan.x.a.m@gmail.com>
2025-06-16 11:46:38 +0700

    Menambahkan menu ekspor pada Pupuk

--- app/Filament/Resources/PupukResource/Pages/ListPupuks.php
+++ app/Filament/Resources/PupukResource/Pages/ListPupuks.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\PupukResource\Pages;
 
-use App\Filament\Resources\PupukResource;
 use Filament\Actions;
+use Filament\Actions\ExportAction;
+use Filament\Actions\StaticAction;
+use App\Filament\Exports\PupukExporter;
+use App\Filament\Resources\PupukResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Exports\Models\Export;
+use Filament\Actions\Exports\Enums\ExportFormat;
 
 class ListPupuks extends ListRecords
 {
@@ -16,6 +21,20 @@ class ListPupuks extends ListRecords
     {
         return [
             Actions\CreateAction::make()->label('Tambah Data Baru'),
+            ExportAction::make()->exporter(PupukExporter::class)
+                ->fileName(fn (Export $export): string => "Pupuk-{$export->getKey()}.xlsx")
+                ->formats([ExportFormat::Xlsx])
+                ->color('success')
+                ->label('Eskpor data')
+                ->modalHeading('Ekspor Data Pupuk')
+                ->modalCancelAction(function (StaticAction $action) {
+                    $action->label('Batalkan');
+                    $action->color('danger');
+                })
+                ->modalSubmitAction(function (StaticAction $action) {
+                    $action->label('Konfirmasi');
+                })
+                ->modalDescription("Silahkan pilih kolom dan sesuaikan namanya.")
         ];
     }
 }
