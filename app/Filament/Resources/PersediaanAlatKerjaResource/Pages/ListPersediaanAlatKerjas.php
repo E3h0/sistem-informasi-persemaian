de44e5476a5ffdbb8c35ef682070b396254f5165
fathan <fathan.x.a.m@gmail.com>
2025-06-16 11:34:52 +0700

    Menambahkan menu ekspor pada PersediaanAlatKerja

--- app/Filament/Resources/PersediaanAlatKerjaResource/Pages/ListPersediaanAlatKerjas.php
+++ app/Filament/Resources/PersediaanAlatKerjaResource/Pages/ListPersediaanAlatKerjas.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\PersediaanAlatKerjaResource\Pages;
 
-use App\Filament\Resources\PersediaanAlatKerjaResource;
 use Filament\Actions;
+use Filament\Actions\ExportAction;
+use Filament\Actions\StaticAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Exports\Models\Export;
+use Filament\Actions\Exports\Enums\ExportFormat;
+use App\Filament\Exports\PersediaanAlatKerjaExporter;
+use App\Filament\Resources\PersediaanAlatKerjaResource;
 
 class ListPersediaanAlatKerjas extends ListRecords
 {
@@ -15,6 +20,20 @@ class ListPersediaanAlatKerjas extends ListRecords
     {
         return [
             Actions\CreateAction::make()->label('Tambah Data Baru'),
+            ExportAction::make()->exporter(PersediaanAlatKerjaExporter::class)
+                ->fileName(fn (Export $export): string => "PersediaanAlatKerja-{$export->getKey()}.xlsx")
+                ->formats([ExportFormat::Xlsx])
+                ->color('success')
+                ->label('Eskpor data')
+                ->modalHeading('Ekspor Data Persediaan Alat Kerja')
+                ->modalCancelAction(function (StaticAction $action) {
+                    $action->label('Batalkan');
+                    $action->color('danger');
+                })
+                ->modalSubmitAction(function (StaticAction $action) {
+                    $action->label('Konfirmasi');
+                })
+                ->modalDescription("Silahkan pilih kolom dan sesuaikan namanya.")
         ];
     }
 }
