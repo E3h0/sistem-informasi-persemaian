3dcf711fadb1c8a1b2dfb37dedb47b49750c98f8
fathan <fathan.x.a.m@gmail.com>
2025-06-16 14:13:04 +0700

    Menambahkan menu ekspor pada PenggunaanPupuk

--- app/Filament/Resources/PenggunaanPupukResource/Pages/ListPenggunaanPupuks.php
+++ app/Filament/Resources/PenggunaanPupukResource/Pages/ListPenggunaanPupuks.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\PenggunaanPupukResource\Pages;
 
-use App\Filament\Resources\PenggunaanPupukResource;
 use Filament\Actions;
+use Filament\Actions\ExportAction;
+use Filament\Actions\StaticAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Exports\Models\Export;
+use Filament\Actions\Exports\Enums\ExportFormat;
+use App\Filament\Exports\PenggunaanPupukExporter;
+use App\Filament\Resources\PenggunaanPupukResource;
 
 class ListPenggunaanPupuks extends ListRecords
 {
@@ -18,6 +23,20 @@ class ListPenggunaanPupuks extends ListRecords
     {
         return [
             Actions\CreateAction::make()->make()->label('Tambah Data Baru'),
+            ExportAction::make()->exporter(PenggunaanPupukExporter::class)
+                ->fileName(fn (Export $export): string => "PenggunaanPupuk-{$export->getKey()}.xlsx")
+                ->formats([ExportFormat::Xlsx])
+                ->color('success')
+                ->label('Eskpor data')
+                ->modalHeading('Ekspor Data Penggunaan Pupuk')
+                ->modalCancelAction(function (StaticAction $action) {
+                    $action->label('Batalkan');
+                    $action->color('danger');
+                })
+                ->modalSubmitAction(function (StaticAction $action) {
+                    $action->label('Konfirmasi');
+                })
+                ->modalDescription("Silahkan pilih kolom dan sesuaikan namanya.")
         ];
     }
 }
